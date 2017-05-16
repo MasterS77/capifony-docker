c617469649c77a09bb83d3073ec22cedc38b8dbd
MasterS77 <stassr@gmail.com>
2017-05-16 20:37:00 +0300

    fix rights for local user

--- cap.sh
+++ cap.sh
@@ -2,16 +2,14 @@
 
 WORKDIR=$(pwd)
 SSH_AGENT=$(readlink -f $SSH_AUTH_SOCK)
-
-if [ $(command -v selinuxenabled >/dev/null 2>&1) -a $(selinuxenabled) ]; then
-    chcon -Rt svirt_sandbox_file_t $WORKDIR $SSH_AGENT
-fi
+UID="$(id -u)"
 
 docker run \
     --rm \
     -i \
     -t \
+    --user $UID \
     -v $WORKDIR:/root/workdir \
     -v $SSH_AGENT:/root/ssh-agent \
-    mjanser/capifony \
+    e238730a7917 \
     $@
